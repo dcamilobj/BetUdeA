227fe45e66dad83e4e81553b8e6f6d6a583c5c95
dcamilobj <dcamilobj@gmail.com>
2017-05-01 13:24:20 -0500

    Se agregan librerias

--- src/co/edu/udea/iw/dao/UsuarioDAO.java
+++ src/co/edu/udea/iw/dao/UsuarioDAO.java
@@ -4,8 +4,9 @@
 package co.edu.udea.iw.dao;
 
 /**
- * @author camilo
- *
+ *Esta interfaz define las operaciones que se pueden hacer sobre la tabla usuario en el sistema.
+ *@author Duban Camilo Bedoya Jiménez
+ *@version 1.0
  */
 public interface UsuarioDAO {
 
