238edcac0c65516bae147e46cfabee629e134bdf
dcamilobj <dcamilobj@gmail.com>
2017-05-15 21:52:04 -0500

    Se inicia con los servicios

---
+++ src/co/edu/udea/iw/ws/SimulacionWS.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package co.edu.udea.iw.ws;
+
+/**
+ * @author camilo
+ *
+ */
+public class SimulacionWS {
+
+}
---
+++ src/co/edu/udea/iw/ws/UsuarioWS.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package co.edu.udea.iw.ws;
+
+import java.rmi.RemoteException;
+import java.util.Date;
+
+/**
+ * @author Duban Camilo Bedoya Jiménez(dcamilo.bedoya@udea.edu.co)
+ * @version 1.0
+ */
+public class UsuarioWS {
+
+	public void registrar(String nombreUsuario, String tipoDocumento,
+			String numeroDocumento, String nombres, String apellidos, 
+			Date fechaNacimiento, String email, String password)
+	{
+	}
+}
