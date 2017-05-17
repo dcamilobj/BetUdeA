589478f740232137594e95b3a82c08a5bd10b8f8
Andres Ceballos Sanchez <andresrxn10@hotmail.com>
2017-05-17 16:12:00 -0500

    Servicio para registrar apuestas funcionando

--- src/co/edu/udea/iw/ws/ApuestaWS.java
+++ src/co/edu/udea/iw/ws/ApuestaWS.java
@@ -4,10 +4,14 @@
 package co.edu.udea.iw.ws;
 
 import java.rmi.RemoteException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -33,14 +37,43 @@ public class ApuestaWS {
 	@Autowired
 	private ApuestaBL apuestaBL;
 	
+	/**
+	 * http://localhost:8080/BetUdeA/BetUdeA/apuesta?evento=Spurs vs Cleveland&fecha=2017-05-18&valor=4000&cuota=2.3&opcion=Spurs&usuario=elver
+	 * @param evento
+	 * @param strFechaEvento
+	 * @param valorApostado
+	 * @param cuota
+	 * @param opcionSeleccionada
+	 * @param usuario
+	 * @throws RemoteException
+	 */
+	@POST
+	@Produces(MediaType.TEXT_PLAIN)
+	public String registrar(@QueryParam("evento") String evento, @QueryParam("fecha") String strFechaEvento,
+						  @QueryParam("valor") Double valorApostado, @QueryParam("cuota") Double cuota,
+						  @QueryParam("opcion") String opcionSeleccionada, @QueryParam("usuario") String usuario) throws RemoteException{
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date fechaEvento = null;
+		try {			
+			fechaEvento = sdf.parse(strFechaEvento);
+			apuestaBL.registrar(evento, fechaEvento, valorApostado, cuota, opcionSeleccionada, usuario);
+		} catch(MyException e) {
+			throw new RemoteException("Error registrando la apuesta");
+		} catch(ParseException e) {
+			throw new RemoteException("Error en la fecha ingresada");
+		}
+		
+		return "Apuesta registrada exitosamente";
+	}
+	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<ApuestaJersey> consultar(@QueryParam("periodo") String periodo) throws RemoteException {
+	public List<ApuestaJersey> consultar(@QueryParam("periodo") Long periodoSimulacion) throws RemoteException {
 		List<Apuesta> apuestas = null;
 		List<ApuestaJersey> resultado = new ArrayList<>();
 		ApuestaJersey apuestaWS = null;
 		try {
-			Long periodoSimulacion = Long.valueOf(periodo);
 			apuestas = apuestaBL.consultar(periodoSimulacion);
 			for(Apuesta apuesta: apuestas) {
 				apuestaWS = new ApuestaJersey();
